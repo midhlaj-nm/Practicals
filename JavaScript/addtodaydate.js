1b42ef25c3798d588865a649d5869e1cea8981d2
Midhlaj vk <midhlajrocks@gmail.com>
2025-02-17 14:20:33 +0530

    Message

--- JavaScript/addtodaydate.js
+++ JavaScript/addtodaydate.js
@@ -2,4 +2,21 @@ const fs = require("fs")
 const path = require("path")
 
 const today = new Date().toLocaleDateString()
-console.log(today)
+// console.log(today)
+const filepath = path.join(__dirname, 'todaysDate.txt');
+
+fs.writeFile(filepath, today, (err) => {
+    if(err){
+        console.log(err)
+    } else {
+        console.log('File Created successfully')
+    }
+})
+
+fs.readFile('todaysDate.txt', 'utf8' ,(err, data) =>{
+    if(err){
+        console.log(err)
+    } else {
+        console.log('File readed successfuly and the data is :',data)
+    }
+})
